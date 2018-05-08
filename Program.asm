634c1a21d898395919a5744e1ec647f14f7a25e0
howardt12345 <howardt12345@users.noreply.github.com>
2018-05-08 14:51:09 -0400

    Adding unary operators

--- Program.asm
+++ Program.asm
@@ -4,9 +4,10 @@ INCLUDE			Irvine32.inc;								//Includes the Irvine32 Library.
 
 .data
 ;															//Strings:
-prompt_1		BYTE	"First Number:  ",		0;				//Prompt for first number.
-prompt_2		BYTE	"Second Number: ",		0;				//Prompt for second number.
-prompt_3		BYTE	"Operator (1 for add, 2 for subtract, 3 for multiply, 4 for divide): ",		0;	//Prompt for operator.
+prompt_1		BYTE	"Operation (1 for add, 2 for subtract, 3 for multiply, 4 for divide): ", 0;	//Prompt for operation.
+prompt_2		BYTE	"First Number:  ",		0;				//Prompt for first number.
+prompt_3		BYTE	"Second Number: ",		0;				//Prompt for second number.
+prompt_4		BYTE	"Number: ",				0;				//Prompt for prime number generation.
 loop_prompt		BYTE	"Would you like to perform another calculation? 1 for YES or 0 for NO: ",	0;	//Prompt for loop.
 invalid			BYTE	"Invalid input.",		0;				//Invalid input.
 cannotDivide	BYTE	"Numbers cannot be negative or zero.",		0;									//Cannot divide numbers error.
@@ -20,13 +21,15 @@ divi			BYTE	" / ", 0;								//String for division symbol.
 dot				BYTE	"."	 , 0;								//String for decimal point.
 
 ;															//Predefined values:
-Operations		DWORD	4		;								//The number of operations.
+totalOpts		DWORD	5		;								//The number of operations.
+binaryOpts		DWORD	4		;								//The number of binary operations.
 precision		DWORD	1000000	;								//The precision of the floating point number result.
 
 ;															//User inputted data:
-firstNum		DWORD	?		;								//The first number.
-secondNum		DWORD	?		;								//The second number.
 opt				DWORD	?		;								//The operator.
+num				DWORD	?		;								//The number (unary operations).
+firstNum		DWORD	?		;								//The first number (binary operations).
+secondNum		DWORD	?		;								//The second number (binary operations).
 ;															//Calculated results:
 bigInt			DWORD	0		;								//The floating point number multiplied by precision.
 result			DWORD	?		;								//The Result.
@@ -41,44 +44,60 @@ temp			DWORD	?		;								//Temporary value used in the calculation of the second
 main	PROC;												//The main procedure.
 	
 	ReadInput:;												//Reading data:
-		MOV		edx,			offset prompt_1;				//Puts memory address of prompt_1 in register edx.
+	GetOperation:;											//Read Operation:
+		MOV		edx,			offset prompt_1;				//Puts memory address of prompt_3 in register edx.
 		CALL	WriteString;									//Writes the String in register edx.
-
 		CALL	ReadInt;										//Reads an integer to register eax.
-		MOV		firstNum,		eax;							//Stores value of eax in firstNum.
-
-		MOV		edx,			offset prompt_2;				//Puts memory address of prompt_2 in register edx.
-		CALL	WriteString;									//Writes the String in register edx.
-
-		CALL	ReadInt;										//Reads an integer to register eax.
-		mov		secondNum,		eax;							//Stores value of eax in secondNum.
-
-		MOV		edx,			offset prompt_3;				//Puts memory address of prompt_3 in register edx.
-		CALL	WriteString;									//Writes the String in register edx.
-
-		CALL	ReadInt;										//Reads an integer to register eax.
-		MOV		opt,			eax;							//Stores value of eax in opt.
+		MOV		opt,			eax;							//Stores value of eax in opt.	
 
 
-	ConditionalJumps:;										//Conditional jumps:
+	CheckOperator:;										//Check the operator:
 		MOV		eax,			opt;							//Puts value of opt in register eax.
-		MOV		ecx,			Operations;						//Puts value of Operations in register ecx.
+		MOV		ebx,			totalOpts;						//Puts value of totalOpts in register ebx.
+		MOV		ecx,			binaryOpts;						//Puts value of binaryOpts in register ecx.
 		MOV		edx,			offset invalid;					//Puts memory address of invalid in register edx.
 ;		//Check if opt is within bounds.
 		CMP		eax,			0;								//Compare eax to 0.
 		JLE		InvalidResponse;								//Jumps to InvalidResponse if less than or equal to 0.
-		CMP		eax,			ecx;							//Compare eax to ecx.
-		JG		InvalidResponse;								//Jumps to InvalidResponse if greater than ecx.
+		CMP		eax,			ebx;							//Compare eax to ebx.
+		JG		InvalidResponse;								//Jumps to InvalidResponse if greater than ebx.
+;		//Checks if opt is binary or unary operation:
+		CMP		eax,			ecx;							//Compare eax to ebx.
+		JLE		BinaryOperations;								//Jumps to BinaryOperations if less than or equal to ecx.
+		JG		UnaryOperations;								//Jumps to UnaryOperations if greater than ecx.
+
+	UnaryOperations:;										//Unary calculator operations:
+;		//Get Number:
+		MOV		edx,			offset prompt_4;				//Puts memory address of prompt_1 in register edx.
+		CALL	WriteString;									//Writes the String in register edx.
+		CALL	ReadInt;										//Reads an integer to register eax.
+		MOV		num,			eax;							//Stores value of eax in num.
+;		//Jump to operation:
+		MOV		eax,			opt;							//Puts value of opt in register eax.
+
+		CMP		eax,			5;								//Compare eax to 5.
+		JE		PrimeGeneration;								//Jump to PrimeGeneration if equal to 5.
+
+	BinaryOperations:;										//Binary calculator operations:
+;		//Get first number:
+		MOV		edx,			offset prompt_2;				//Puts memory address of prompt_1 in register edx.
+		CALL	WriteString;									//Writes the String in register edx.
+		CALL	ReadInt;										//Reads an integer to register eax.
+		MOV		firstNum,		eax;							//Stores value of eax in firstNum.
+;		//Get second number:
+		MOV		edx,			offset prompt_3;				//Puts memory address of prompt_2 in register edx.
+		CALL	WriteString;									//Writes the String in register edx.
+		CALL	ReadInt;										//Reads an integer to register eax.
+		mov		secondNum,		eax;							//Stores value of eax in secondNum.
+;		//Jump to operation:
+		MOV		eax,			opt;							//Puts value of opt in register eax.
 
 		CMP		eax,			1;								//Compare eax to 1.
 		JE		Addition;										//Jump to Addition if equal to 1.
-
 		CMP		eax,			2;								//Compare eax to 2.
 		JE		Subtraction;									//Jump to Subtraction if equal to 2.
-
 		CMP		eax,			3;								//Compare eax to 3.
 		JE		Multiplication;									//Jump to Multiplication if equal.
-
 		CMP		eax,			4;								//Compare eax to 4.
 		JE		Division;										//Jump to Division if equal.
 
@@ -136,7 +155,7 @@ main	PROC;												//The main procedure.
 		MOV		edx,			offset multi;					//Puts memory address of multi in register edx.
 		CALL	WriteString;									//Writes the String in register edx.
 		MOV		eax,			secondNum;						//Puts value of secondNum in register eax.
-		CALL	WriteInt;										//Writes the integer in register eax.
+		CALL	WriteInt;										//Writes the integer.
 		MOV		edx,			offset equals;					//Puts memory address of equals in register edx.
 		CALL	WriteString;									//Writes the String in register edx.
 ;		//Print result:
@@ -198,6 +217,13 @@ main	PROC;												//The main procedure.
 		CALL	CrLf;											//New Line.
 		JMP		JumpToLoop;										//Jump to JumpToLoop.
 
+
+	PrimeGeneration:;										//Prime number generation:
+		MOV		eax,			num;							//Puts value of num in register eax.
+		CALL	WriteInt;										//Writes the integer in register eax.
+;		//Jump to loop.
+		CALL	CrLf;											//New Line.
+		JMP		JumpToLoop;										//Jump to JumpToLoop.
 
 	JumpToLoop:;											//Loops until user quits:
 		MOV		edx,			offset loop_prompt;				//Puts memory address of loop_response in register edx.
